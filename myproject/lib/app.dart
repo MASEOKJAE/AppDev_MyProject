8b869c34df3b591805b634a74ab242b358ae6fb9
MASEOKJAE <maasj7514@gmail.com>
2023-11-18 21:06:03 +0900

    first setting

---
+++ myproject/lib/app.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+// import 'view/home.dart';
+// import 'view/login.dart';
+// import 'view/profile.dart';
+// import 'view/add.dart';
+// import 'view/detail.dart';
+// import 'view/modify.dart';
+// import 'view/wishlist.dart';
+
+// TODO: Convert ShrineApp to stateful widget (104)
+class StudyApp extends StatelessWidget {
+  const StudyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '',
+      initialRoute: '/login',
+      routes: {
+        // '/login': (BuildContext context) => const LoginPage(),
+        // '/': (BuildContext context) => const HomePage(),
+        // '/add': (BuildContext context) => const AddPage(),
+        // '/profile': (BuildContext context) => const Profile(),
+        // '/detail': (BuildContext context) => const DetailPage(),
+        // '/modify': (BuildContext context) => const ModifyPage(),
+        // '/wishlist': (BuildContext context) => const WishListPage(),
+      },
+      // TODO: Customize the theme (103)
+      theme: ThemeData.light(useMaterial3: false),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ myproject/lib/main.dart
@@ -0,0 +1,27 @@
+// import 'package:finalterm_project/firebase_options.dart';
+// import 'package:finalterm_project/model/product_repository.dart';
+// import 'package:finalterm_project/model/user_repository.dart';
+import 'package:finalterm_project/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart'; // new
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '/app.dart';
+
+void main() async {
+  // 이 부분 추가
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(
+    MultiProvider(
+    providers: [
+      // ChangeNotifierProvider(create: (_) => ProductRepository()),
+      // ChangeNotifierProvider(create: (_) => UserRepository()),
+    ],
+    builder: ((context, child) => const StudyApp()),
+  ));
+}
